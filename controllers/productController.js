9292f86a9f986adac0e1f70b639529cd429566d9
Yash Namdeo <yashnamdeo754@gmail.com>
2025-05-25 22:36:47 +0530

    initial commit

---
+++ controllers/productController.js
@@ -0,0 +1,85 @@
+import {v2 as cloudinary} from 'cloudinary';
+import productModel from '../models/productModel.js';
+
+// Function for add product
+
+const addProduct = async(req,res)=>{
+    try {
+        const{name,description,price,category,subCategory,sizes,bestseller } = req.body;
+
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+
+        const images = [image1,image2,image3,image4].filter((item)=> item !== undefined);
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item)=>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'})
+                return result.secure_url;
+            })
+        );
+
+        const productData = {
+            name,
+            description,
+            category,
+            price:Number(price),
+            subCategory,
+            bestseller: bestseller === "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now()
+        }
+
+        console.log(productData);
+        const product = new productModel(productData);
+
+        await product.save();
+
+        
+        res.json({
+            success: true,
+            message: "Product added successfully",
+            product
+        })
+        
+        
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message:error.message
+        })
+    }
+}
+
+
+// Function to list products
+
+const listProducts = (req,res)=>{
+    
+}
+
+
+// Function to remove product
+
+const removeProduct = (req,res)=>{
+
+}
+
+
+// Function to display single product
+
+const singleProduct = (req,res)=>{
+
+}
+
+
+export {
+    addProduct,
+    listProducts,
+    removeProduct,
+    singleProduct
+};
---
+++ controllers/userController.js
@@ -0,0 +1,101 @@
+import validator from "validator";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import userModel from "../models/userModel.js";
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
+
+//Route for user Login
+
+const loginUser = async(req,res) =>{
+    try {
+        
+        const {email, password} = req.body;
+
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({
+                success:false, message:"User doesn't exists"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(isMatch){
+            const token = createToken(user._id);
+            return res.json({
+                success:true,
+                message:"User logged in successfully",
+                token
+            });
+        }else{
+            return res.json({
+                success:false,
+                message:"Invalid credentials" 
+            })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success:false, message:error.message});
+    }
+}
+//Route for user Registration
+
+const registerUser = async(req,res) =>{
+    try{
+        const { name, email, password } = req.body;
+
+        //checking user already exists 
+        const exists = await userModel.findOne({email});
+
+        if(exists){
+            return res.json({
+                success:false, message:"User already exists"
+            })
+        }
+
+        //validating email format & password
+        if(!validator.isEmail(email)){
+            return res.json({success:false, message:"Please enter a valid email format"});
+        }
+        if(password.length < 8){
+            return res.json({success:false, message:"Please enter a strong password with at least 8 characters"});
+        }
+
+        //hashing password
+
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //creating user
+
+        const newUser  = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        const user = await newUser.save();
+
+        const token = createToken(user._id);
+
+        res.json({
+            success: true,
+            message: "User registered successfully",token})
+    }catch(error) {
+        res.status(500).json({success:false, message: "Internal Server Error" });
+    }
+}
+
+//Route for Admin Login
+
+const adminLogin = async(req,res) =>{
+
+}
+
+export { loginUser, registerUser, adminLogin };
