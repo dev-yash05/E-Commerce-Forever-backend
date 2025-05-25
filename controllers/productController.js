723e324fedeb13efcac92c4d6470921230061c66
Yash Namdeo <yashnamdeo754@gmail.com>
2025-05-25 23:06:26 +0530

    added list products, remove product, and display single product functionality in productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -58,21 +58,73 @@ const addProduct = async(req,res)=>{
 
 // Function to list products
 
-const listProducts = (req,res)=>{
-    
+const listProducts = async(req,res)=>{
+
+    try {
+        
+        const products = await productModel.find({});
+        res.json({
+            success: true,
+            products
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        });
+        
+    }
+
 }
 
 
 // Function to remove product
 
-const removeProduct = (req,res)=>{
-
+const removeProduct = async(req,res)=>{
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({
+            success: true,
+            message: "Product removed successfully"
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        });
+        
+    }
 }
 
 
 // Function to display single product
 
-const singleProduct = (req,res)=>{
+const singleProduct = async(req,res)=>{
+
+    try {
+        const{productId} = req.body;
+        const product = await productModel.findById(productId);
+        if(!product){
+            return res.json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+        res.json({
+            success: true,
+            product
+        });
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        });
+        
+    }
 
 }
 
