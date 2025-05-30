d3abe2d2886c21ed8652c96ec59e84ae928c3567
Yash Namdeo <yashnamdeo754@gmail.com>
2025-05-30 19:57:44 +0530

    created cart routes:- addToCart,updateCart,getUserCart, and authentication middleware for cart routes

---
+++ controllers/cartController.js
@@ -0,0 +1,105 @@
+
+
+//Add products to user cart
+
+import userModel from "../models/userModel";
+
+const addToCart = async (req, res) => {
+    try {
+        
+        const {userId,itemId,size} = req.body;
+
+        const userData = await userModel.findById(userId);
+        let cartData = await userData.cartData;
+
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] += 1; // Increment quantity if item already exists
+            }else{
+                cartData[itemId][size] = 1; // Add new size with quantity 1
+            }
+        }else{
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1;
+        }
+
+        await userModel.findByIdAndUpdate(userId,{cartData});
+
+        res.json({
+            success: true,
+            message: 'Item added to cart successfully'
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        });
+        
+    }
+}
+
+//update user cart
+const updateCart = async (req, res) => {
+
+try {
+    
+const {userId,itemId,size,quantity} = req.body;
+const userData = await userModel.findById(userId);
+let cartData = await userData.cartData;
+
+cartData[itemId][size] = quantity; // Update the quantity of the item
+
+await userModel.findByIdAndUpdate(userId,{cartData});
+
+res.json({
+    success: true,
+    message: 'Cart updated successfully'
+});
+
+
+} catch (error) {
+    console.log(error);
+    res.json({
+        success: false,
+        message: error.message
+    });
+}
+
+}
+
+
+//get user cart
+const getUserCart = async (req, res) => {
+
+ try {
+    
+    const{userId} = req.body;
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
+
+    res.json({
+        success: true,
+        message: 'Cart fetched successfully',
+        cartData
+    });
+
+ } catch (error) {
+    console.log(error);
+    res.json({
+        success: false,
+        message: error.message
+    });
+ }
+
+}
+
+
+export {
+    addToCart,
+    updateCart,
+    getUserCart
+}
