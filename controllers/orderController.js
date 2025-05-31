d94fe2e5c02307773e0fa0347702c829b8f3d326
Yash Namdeo <yashnamdeo754@gmail.com>
2025-05-31 18:54:24 +0530

    created orderModel for managing orders

---
+++ controllers/orderController.js
@@ -0,0 +1,44 @@
+// placing orders using COD method
+
+const placeOrder = async (req, res) => {
+    
+}
+const placeOrderStripe = async (req, res) => {
+
+}
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+
+//All orders data for admin panel
+
+const allOrders = async (req, res) => {
+
+}
+
+
+//user orders data for frontend panel
+
+const userOrders = async (req, res) => {
+
+}
+
+
+//update order status by admin
+
+const updateStatus = async (req, res) => {
+
+}
+
+
+
+
+export {
+    placeOrder,
+    placeOrderStripe,
+    placeOrderRazorpay,
+    allOrders,
+    userOrders,
+    updateStatus
+}
