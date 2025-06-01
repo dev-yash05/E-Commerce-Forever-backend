8bd1da2a52d772fa03426e7b7742b2325dca5773
Yash Namdeo <yashnamdeo754@gmail.com>
2025-06-01 13:25:32 +0530

    created allOrders endpoint for admin panel

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -47,6 +47,20 @@ const placeOrderRazorpay = async (req, res) => {
 
 const allOrders = async (req, res) => {
 
+    try {
+        const orders = await orderModel.find({});
+        res.json({
+            success: true,
+            orders
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+
 }
 
 
